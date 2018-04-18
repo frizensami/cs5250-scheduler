2f69f58ea94bbf5a89aac40fcd3e5a5d2d818fb6
Sriram Sami <frizensami@gmail.com>
2018-04-19 00:38:20 +0800

    Add SRTF test2, ignore report folder

--- test.sh
+++ test.sh
@@ -18,3 +18,12 @@ cmp RR.txt "$testfolder/output-$test1-RR.txt" && echo "Test RR good" || echo "Te
 cmp SRTF.txt "$testfolder/output-$test1-SRTF.txt" && echo "Test SRTF good" || echo "Test SRTF FAIL"
 echo "-----------------"
 echo ""
+
+test1="SRTF-test2"
+echo "-----------------"
+echo "Performing $test1 tests..."
+python simulator.py "$testfolder/input-$test1.txt" > /dev/null
+cmp RR.txt "$testfolder/output-$test1-RR.txt" && echo "Test RR good" || echo "Test RR FAIL"
+cmp SRTF.txt "$testfolder/output-$test1-SRTF.txt" && echo "Test SRTF good" || echo "Test SRTF FAIL"
+echo "-----------------"
+echo ""
